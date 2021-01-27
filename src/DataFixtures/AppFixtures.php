2283b814b88a086811e525f9aaaf6e4535622aea
Nux007 <007.nux@gmail.com>
2021-01-27 18:53:13 +0100

    Login and configuration.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,32 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+    private $passwordEncoder;
 
+    /**
+     * AppFixtures constructor.
+     * @param UserPasswordEncoderInterface $pInterface
+     */
+    public function __construct(UserPasswordEncoderInterface $pInterface){
+        $this->passwordEncoder = $pInterface;
+    }
+
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager) {
+        $user = new User();
+        $user->setEmail('admin@eb.com');
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'password'));
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
