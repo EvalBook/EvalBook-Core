c6316fa12d5d8ae92cf3ab7a46b6513fe0990194
Nux007 <007.nux@gmail.com>
2021-01-28 06:45:43 +0100

    Moved custom bootstrap width to the right folder.

--- assets/app.js
+++ assets/app.js
@@ -14,6 +14,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // importing scss files to compile.
 import './scss/bootstrap/bootstrap-ebsecondary.scss';
 import './scss/bootstrap/bootstrap-ebprimary.scss';
+import './scss/bootstrap/bootstrap-custom-width.scss';
 import './scss/app.scss';
 
 
