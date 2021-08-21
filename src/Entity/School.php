fb6c7a5d5e5a2c28823df52cc410057cfbae4535
Nux007 <007.nux@gmail.com>
2021-08-21 12:42:36 +0200

    Updated school entity.

--- src/Entity/School.php
+++ src/Entity/School.php
@@ -15,78 +15,130 @@ class School
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
      */
-    private $id;
+    private int $id;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $name;
+    private string $name;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $street;
+    private string $street;
 
     /**
      * @ORM\Column(type="smallint", length=255)
      */
-    private $number;
+    private int $number;
+
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $city;
+    private string $city;
 
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private string $zip;
+
+
+    /**
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * @return string|null
+     */
     public function getName(): ?string
     {
         return $this->name;
     }
 
+    /**
+     * @param string $name
+     * @return $this
+     */
     public function setName(string $name): self
     {
         $this->name = $name;
-
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getStreet(): ?string
     {
         return $this->street;
     }
 
+    /**
+     * @param string $street
+     * @return $this
+     */
     public function setStreet(string $street): self
     {
         $this->street = $street;
-
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getNumber(): ?string
     {
         return $this->number;
     }
 
+    /**
+     * @param string $number
+     * @return $this
+     */
     public function setNumber(string $number): self
     {
         $this->number = $number;
-
         return $this;
     }
 
+    /**
+     * @return string|null
+     */
     public function getCity(): ?string
     {
         return $this->city;
     }
 
+    /**
+     * @param string $city
+     * @return $this
+     */
     public function setCity(string $city): self
     {
         $this->city = $city;
+        return $this;
+    }
 
+    /**
+     * @return string|null
+     */
+    public function getZip(): ?string
+    {
+        return $this->zip;
+    }
+
+    /**
+     * @param string $zip
+     * @return $this
+     */
+    public function setZip(string $zip): self
+    {
+        $this->zip = $zip;
         return $this;
     }
 }
