04c05e290beaa8a5a8855c72d14d406be505469d
Nux007 <007.nux@gmail.com>
2021-09-04 07:36:31 +0200

    Generating .env file with an sf command, adding database dsn.

--- src/Command/RegenerateEnvCommand.php
+++ src/Command/RegenerateEnvCommand.php
@@ -18,11 +18,12 @@ class RegenerateEnvCommand extends Command
 {
 
     /**
-     * Command confirguration, specifying console argvs.
+     * Command configuration, specifying console argv.
      */
     protected function configure(): void
     {
         $this->addArgument('env', InputArgument::REQUIRED, 'The desired environnement for .env generation');
+        $this->addArgument('db_dsn', InputArgument::REQUIRED, 'The database connexion DSN');
     }
 
     /**
@@ -35,13 +36,17 @@ class RegenerateEnvCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
         $env = $input->getArgument('env');
+        $dbDsn = $input->getArgument('db_dsn');
 
-        if(in_array($env, ['prod', 'dev'])) {
-            $io->note(sprintf('Generating .env file for environnement: %s', $env));
-        }
-        else {
+        // Checking env file environnement.
+        if(!in_array($env, ['prod', 'dev'])) {
             $io->error("Environnement should be \"dev\" or \"prod\"");
             exit(1);
+        }
+
+        // Checking dsn.
+        if(!preg_match("#[a-zA-Z]*:\/\/\S*:\S*@\S*:\d*\/\S*#", $dbDsn)) {
+            $io->error("The provided database DSN does not match the dsn pattern");
         }
 
         // Processing env file.
@@ -52,20 +57,26 @@ class RegenerateEnvCommand extends Command
             $proceed = unlink($file);
         }
 
-        if($proceed) {
-            // Generating app secret.
-            $env_data = "APP_ENV=$env\nAPP_SECRET=";
-            for ($i = 0; $i < 32; $i++) {
-                $env_data .= '0123456789abcdef'[rand(0, 15)];
-            }
-
-            file_put_contents($file, $env_data);
-            // Pushing data into a new env file.
-        }
-        else {
+        if(!$proceed) {
             $io->error("Unable to delete old \"$file\" file, please delete it manually and try again");
             exit(1);
         }
+
+        $io->note("New env file created");
+        $io->note(sprintf('Generating .env file for environnement: %s', $env));
+
+        // Generating app secret.
+        $env_data = "APP_ENV=$env\nAPP_SECRET=";
+        for ($i = 0; $i < 32; $i++) {
+            $env_data .= '0123456789abcdef'[rand(0, 15)];
+        }
+
+        $io->note("Storing APP_ENV and APP_SECRET");
+        file_put_contents($file, $env_data . "\n", FILE_APPEND);
+
+        // Generating databse dsn.
+        $io->note("Storing Database dsn");
+        file_put_contents($file, 'DATABASE_URL=' . "\"$dbDsn\"", FILE_APPEND);
 
         // Generating app secret.
         $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
