540e79e9c422ea15573d934bd56f2d2b60452082
Nux007 <007.nux@gmail.com>
2022-01-20 05:50:46 +0100

    Delete old installation env files before starting a new one.

--- src/Command/CommandUtil.php
+++ src/Command/CommandUtil.php
@@ -4,6 +4,8 @@ namespace EvalBookCore\Command;
 
 class CommandUtil {
 
+    private const WORKING_DIR = __DIR__ . '/../../';
+
     /**
      * @param string $cmd
      * @param bool $debug
@@ -12,8 +14,7 @@ class CommandUtil {
     public static function execSymfonyCmd(string $cmd, bool $debug=false): bool
     {
         $debug = true;
-        $workingDir = __DIR__ . '/../../';
-        exec('cd "'. $workingDir . '" && ' . $cmd, $output, $code);
+        exec('cd "'. self::WORKING_DIR . '" && ' . $cmd, $output, $code);
 
         if($debug) {
             echo "<pre>";
@@ -21,10 +22,35 @@ class CommandUtil {
             echo "</pre>";
 
             echo "<pre>";
-            var_dump('cd "' . $workingDir . '" && ' . $cmd);
+            var_dump('cd "' . self::WORKING_DIR . '" && ' . $cmd);
             echo "</pre>";
         }
         return $code === 0;
+    }
+
+
+    /**
+     * Prepare env file for installation, check if env files exists and reset them if so.
+     * @param string $env
+     * @return void
+     */
+    public static function prepareEnvFiles(string $env): void
+    {
+        // Make sure .env file exists, it avoid Symfony to trigger an env file load error.
+        $files = [
+            self::WORKING_DIR . '.env',
+            self::WORKING_DIR . '.env.local',
+            self::WORKING_DIR . '.env.test',
+        ];
+
+        array_map(function($f){if(file_exists($f)) unlink($f);}, $files);
+
+        if(strtolower($env) === 'prod'){
+            CommandUtil::execSymfonyCmd("echo \"DATABASE_URL=''\" >> .env");
+        }
+        elseif(strtolower($env) === 'dev'){
+            CommandUtil::execSymfonyCmd("echo \"DATABASE_URL=''\" >> .env.local");
+        }
     }
 
 }
